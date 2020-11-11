5b1a7cfef2685304297950e2959144941dc0e691
Vale Trujillo <vale.trujillo@gmail.com>
2020-11-11 13:57:06 -0800

    initial commit

---
+++ setup.R
@@ -0,0 +1,12 @@
+#install.packages("devtools")
+devtools::install_github("nstrayer/datadrivencv")
+
+# run ?datadrivencv::use_datadriven_cv to see more details
+datadrivencv::use_datadriven_cv(
+  full_name = "Vale R. Trujillo",
+  data_location = "https://docs.google.com/spreadsheets/d/14MQICF2F8-vf8CKPF1m4lyGKO6_thG-4aSwat1e2TWc"
+)
+
+rmarkdown::render("cv.rmd",
+                  params = list(pdf_mode = FALSE),
+                  output_file = "cv.html")
