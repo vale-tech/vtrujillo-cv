ad4dc9c764c249d5a550b08383bff3e4fbf42c6b
Vale Trujillo <vale.trujillo@gmail.com>
2022-10-19 00:17:58 +0000

    Fixed R extension container setup and modified res output file

--- render_cv.r
+++ render_cv.r
@@ -8,7 +8,7 @@
 # Knit the HTML version
 rmarkdown::render("cv.rmd",
                   params = list(pdf_mode = FALSE),
-                  output_file = "output/index.html")
+                  output_file = "output/Trujillo V Resume.html")
 
 # Knit the PDF version to temporary html location
 tmp_html_cv_loc <- fs::file_temp(ext = ".html")
